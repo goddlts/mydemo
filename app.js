cb7c5ba63413e457dd144630846f25d7bc16725b
牛亮 <nll@itcast.cn>
2018-06-26 00:30:40 +0800

    配置mysql

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const router = require('./router');
 const expressArtTemplate = require('express-art-template');
+const bodyParser = require('body-parser');
+
 const app = express();
 app.listen(3000, () => {
   console.log('开启端口 3000');
 });
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
 // 配置模板引擎
 app.engine('html', expressArtTemplate);
 // 配置静态资源
 app.use('/public', express.static('./public'));
-app.use('/node_modules', express.static('./node_modules'))
+app.use('/node_modules', express.static('./node_modules'));
 
 
 // 4 app.js中使用app.use 挂载路由对象
--- controller/user.js
+++ controller/user.js
@@ -1,5 +1,13 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'root',
+  database: 'mydemo'
+});
+
 exports.showSignin = (req, res) => {
-  res.send('showSignin');
+  res.render('signin.html');
 };
 
 exports.handleSignin = (req, res) => {
@@ -7,11 +15,21 @@ exports.handleSignin = (req, res) => {
 };
 
 exports.showSignup = (req, res) => {
-
+  res.render('signup.html');
 };
 
 exports.handleSignup = (req, res) => {
-
+  // 注册
+  const sql = 'insert into user set ?'
+  connection.query(sql, req.body, (err, results) => {
+    if (err) {
+      res.send('插入数据失败');
+      return;
+    }
+    res.send({
+      success: true
+    });
+  });
 };
 
 exports.handleSignout = (req, res) => {
