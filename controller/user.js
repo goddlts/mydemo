9191fa28b0d84e414677af97898e0c90bc20b3ce
goddlts <54337467@qq.com>
2018-06-26 15:27:24 +0800

    添加model

--- controller/user.js
+++ controller/user.js
@@ -1,11 +1,5 @@
-const mysql = require('mysql');
 const moment = require('moment');
-const connection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'root',
-  database: 'ithub'
-});
+const User = require('../model/user');
 
 exports.showSignin = (req, res) => {
   res.render('signin.html');
@@ -33,12 +27,23 @@ exports.showSignup = (req, res) => {
 };
 
 exports.handleSignup = (req, res) => {
-  // 数据验证
-  // 验证是否为空
-  connection.query(
-    'select * from `users` where `email`=?',
-    req.body.email,
-    (err, results) => {
+  User.getByEMail(req.body.email, (err, user) => {
+    if (err) {
+      res.json({
+        code: 500,
+        msg: err.message
+      });
+      return;
+    }
+    if (user) {
+      res.json({
+        code: 401,
+        msg: '邮箱地址已被占用'
+      });
+      return;
+    }
+
+    User.getByNickname(req.body.nickname, (err, user) => {
       if (err) {
         res.json({
           code: 500,
@@ -46,53 +51,30 @@ exports.handleSignup = (req, res) => {
         });
         return;
       }
-      if (results.length > 0) {
+      if (user) {
         res.json({
           code: 401,
-          msg: '邮箱地址已被占用'
+          msg: '昵称已被占用'
         });
         return;
       }
-      // 邮箱没有被占用
-      connection.query(
-        'select * from `users` where `nickname`=?',
-        req.body.nickname,
-        (err, results) => {
-          if (err) {
-            res.json({
-              code: 500,
-              msg: err.message
-            });
-            return;
-          }
-          if (results.length > 0) {
-            res.json({
-              code: 401,
-              msg: '昵称已被占用'
-            });
-            return;
-          }
-          // 验证通过此处去注册
-          // 注册
-          const sql = 'insert into users set ?';
-          req.body.createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
-          connection.query(sql, req.body, (err, results) => {
-            if (err) {
-              res.json({
-                code: 500,
-                msg: '服务器内部错误'
-              });
-              return;
-            }
-            res.json({
-              code: 200,
-              msg: '注册成功'
-            });
+
+      req.body.createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+      User.signup(req.body, (err) => {
+        if (err) {
+          res.json({
+            code: 500,
+            msg: err.message
           });
+          return;
         }
-      )
-    }
-  )
+        res.json({
+          code: 200,
+          msg: '注册成功'
+        });
+      });
+    });
+  });
 };
 
 exports.handleSignout = (req, res) => {
---
+++ model/user.js
@@ -0,0 +1,51 @@
+const db = require('./db-helper');
+
+exports.getByEMail = (email, callback) => {
+  db.query(
+    'select * from `users` where `email`=?',
+    email,
+    (err, results) => {
+      if (err) {
+        callback(err);
+        return;
+      }
+      if (results.length > 0) {
+        callback(null, results[0]);
+      } else {
+        callback(null, null);
+      }
+    }
+  );
+};
+
+exports.getByNickname = (nickname, callback) => {
+  db.query(
+    'select * from `users` where `nickname`=?',
+    nickname,
+    (err, results) => {
+      if (err) {
+        callback(err);
+        return;
+      }
+      if (results.length > 0) {
+        callback(null, results[0]);
+      } else {
+        callback(null, null);
+      }
+    }
+  )
+};
+
+exports.signup = (user, callback) => {
+  db.query(
+    'insert into `users` set ?',
+    user,
+    (err, results) => {
+      if (err) {
+        callback(err);
+        return;
+      }
+      callback(null);
+    }
+  );
+}
