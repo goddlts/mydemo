6ce8396a5bf61896b9c21e00998be875e8b1ed08
goddlts <54337467@qq.com>
2018-06-26 14:30:19 +0800

    注册功能

--- controller/user.js
+++ controller/user.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql');
+const moment = require('moment');
 const connection = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
   password: 'root',
-  database: 'mydemo'
+  database: 'ithub'
 });
 
 exports.showSignin = (req, res) => {
@@ -11,7 +12,20 @@ exports.showSignin = (req, res) => {
 };
 
 exports.handleSignin = (req, res) => {
-
+  const sql = 'insert into users set ?';
+  connection.query(sql, req.body, (err, results) => {
+    if (err) {
+      res.json({
+        code: 500,
+        msg: '服务器内部错误'
+      });
+      return;
+    }
+    res.json({
+      code: 200,
+      msg: '注册成功'
+    });
+  });
 };
 
 exports.showSignup = (req, res) => {
@@ -19,19 +33,67 @@ exports.showSignup = (req, res) => {
 };
 
 exports.handleSignup = (req, res) => {
-  // 注册
-  const sql = 'insert into user set ?'
-  connection.query(sql, req.body, (err, results) => {
-    if (err) {
-      res.send('插入数据失败');
-      return;
+  // 数据验证
+  // 验证是否为空
+  connection.query(
+    'select * from `users` where `email`=?',
+    req.body.email,
+    (err, results) => {
+      if (err) {
+        res.json({
+          code: 500,
+          msg: err.message
+        });
+        return;
+      }
+      if (results.length > 0) {
+        res.json({
+          code: 401,
+          msg: '邮箱地址已被占用'
+        });
+        return;
+      }
+      // 邮箱没有被占用
+      connection.query(
+        'select * from `users` where `nickname`=?',
+        req.body.nickname,
+        (err, results) => {
+          if (err) {
+            res.json({
+              code: 500,
+              msg: err.message
+            });
+            return;
+          }
+          if (results.length > 0) {
+            res.json({
+              code: 401,
+              msg: '昵称已被占用'
+            });
+            return;
+          }
+          // 验证通过此处去注册
+          // 注册
+          const sql = 'insert into users set ?';
+          req.body.createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+          connection.query(sql, req.body, (err, results) => {
+            if (err) {
+              res.json({
+                code: 500,
+                msg: '服务器内部错误'
+              });
+              return;
+            }
+            res.json({
+              code: 200,
+              msg: '注册成功'
+            });
+          });
+        }
+      )
     }
-    res.send({
-      success: true
-    });
-  });
+  )
 };
 
 exports.handleSignout = (req, res) => {
-
 };
